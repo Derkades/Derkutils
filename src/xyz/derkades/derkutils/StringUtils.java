d6a46d6c9c90166cae36bb1e187dcc5ff5e97f68
Derkades <robinslot@hotmail.nl>
2018-07-21 17:03:20 +0200

    fix typo

--- src/xyz/derkades/derkutils/StringUtils.java
+++ src/xyz/derkades/derkutils/StringUtils.java
@@ -47,7 +47,7 @@ public class StringUtils {
 	}
 	
 	/**
-	 * Adds <i>append</i> to <i>string</p> if <i>string</i> does not end with <i>append</i>.
+	 * Adds <i>append</i> to <i>string</i> if <i>string</i> does not end with <i>append</i>.
 	 * @param string
 	 * @param append
 	 * @return
