1ca933c7d291ac3218e69998dcfadeefab930bb6
Robin <robinslot@hotmail.nl>
2019-06-18 00:28:52 +0200

    Add method for placeholders without papi

--- src/xyz/derkades/derkutils/bukkit/Chat.java
+++ src/xyz/derkades/derkutils/bukkit/Chat.java
@@ -72,7 +72,7 @@ public class Chat {
 	 * @param section Location of the message in the config
 	 * @return Formatted base component
 	 */
-	public static BaseComponent[] toComponentWithPlaceholders(final FileConfiguration config, final String path, final Player player, final Placeholder... placeholders) {
+	public static BaseComponent[] toComponentWithPapiPlaceholders(final FileConfiguration config, final String path, final Player player, final Placeholder... placeholders) {
 		final ComponentBuilder builder = new ComponentBuilder("");
 
 		config.getList(path).stream()
@@ -87,6 +87,58 @@ public class Chat {
 				builder.append(Colors.toComponent(PlaceholderUtil.parsePapiPlaceholders(
 						map.get("text"),
 						player, placeholders)), FormatRetention.NONE);
+
+				map.entrySet().stream().filter(e -> !e.getKey().equals("text")).forEach(e -> {
+					final String k = e.getKey();
+					final String v = e.getValue();
+
+					if (k.equals("hover")) {
+						builder.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Colors.toComponent(v)));
+					} else if (k.equals("url")) {
+						builder.event(new ClickEvent(ClickEvent.Action.OPEN_URL, v));
+					} else if (k.equals("run_command")) {
+						builder.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, v));
+					} else if (k.equals("suggest_command")) {
+						builder.event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, v));
+					} else if (k.equals("color")) {
+						builder.color(ChatColor.valueOf(v.toUpperCase()));
+					} else if (k.equals("bold")) {
+						builder.bold(Boolean.parseBoolean(v));
+					} else if (k.equals("underlined")) {
+						builder.underlined(Boolean.parseBoolean(v));
+					} else if (k.equals("italic")) {
+						builder.italic(Boolean.parseBoolean(v));
+					} else if (k.equals("obfuscated")) {
+						builder.obfuscated(Boolean.parseBoolean(v));
+					} else if (k.equals("insert") || k.equals("chat_append")) {
+						builder.insertion(v);
+					} else {
+						throw new IllegalArgumentException(String.format("Unsupported option for message %s: %s", k, v));
+					}
+				});
+			});
+		return builder.create();
+	}
+
+	/**
+	 * TODO example
+	 * @param section Location of the message in the config
+	 * @return Formatted base component
+	 */
+	public static BaseComponent[] toComponentWithPlaceholders(final FileConfiguration config, final String path, final Placeholder... placeholders) {
+		final ComponentBuilder builder = new ComponentBuilder("");
+
+		config.getList(path).stream()
+			.filter(s -> s instanceof Map<?, ?>)
+			.forEach((s) -> {
+				@SuppressWarnings("unchecked")
+				final Map<String, String> map = ((Map<String, String>) s);
+				if (!map.containsKey("text")) {
+					throw new IllegalArgumentException("Every list entry must contain a message");
+				}
+
+				builder.append(Colors.toComponent(PlaceholderUtil.parsePlaceholders(
+						map.get("text"), placeholders)), FormatRetention.NONE);
 
 				map.entrySet().stream().filter(e -> !e.getKey().equals("text")).forEach(e -> {
 					final String k = e.getKey();
