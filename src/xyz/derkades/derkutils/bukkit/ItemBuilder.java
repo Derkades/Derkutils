79e87423e9a842f23b7020735c2d310b7b90e0c0
Robin <robinslot@hotmail.nl>
2019-07-28 13:03:54 +0100

    Add placeholder methods to ItemBuilder

--- src/xyz/derkades/derkutils/bukkit/ItemBuilder.java
+++ src/xyz/derkades/derkutils/bukkit/ItemBuilder.java
@@ -3,17 +3,22 @@ package xyz.derkades.derkutils.bukkit;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import de.tr7zw.nbtapi.NBTItem;
 import xyz.derkades.derkutils.bukkit.reflection.ReflectionUtil;
 
 public class ItemBuilder implements Serializable {
@@ -23,7 +28,7 @@ public class ItemBuilder implements Serializable {
 	private ItemStack item;
 	
 	public ItemBuilder(final Material material){
-		item = new ItemStack(material);
+		this.item = new ItemStack(material);
 	}
 	
 	public ItemBuilder(final ItemStack item){
@@ -31,117 +36,197 @@ public class ItemBuilder implements Serializable {
 	}
 	
 	public ItemBuilder(final OfflinePlayer skullOwner){
-		item = new ItemBuilder(Material.PLAYER_HEAD).skullOwner(skullOwner).create();
+		this.item = new ItemBuilder(Material.PLAYER_HEAD).skullOwner(skullOwner).create();
 	}
 	
 	public ItemBuilder amount(final int amount){
-		item.setAmount(amount);
+		this.item.setAmount(amount);
 		return this;
 	}
 	
 	public ItemBuilder name(final String name){
-		final ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = this.item.getItemMeta();
 		meta.setDisplayName(name);
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 	
 	public ItemBuilder coloredName(final String name){
-		final ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = this.item.getItemMeta();
 		meta.setDisplayName(Colors.parseColors(name));
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 	
 	public ItemBuilder lore(final String... lore){
-		final ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = this.item.getItemMeta();
 		meta.setLore(Arrays.asList(lore));
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 	
 	public ItemBuilder coloredLore(final String... lore){
-		final ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = this.item.getItemMeta();
 		meta.setLore(Colors.parseColors(Arrays.asList(lore)));
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 	
 	public ItemBuilder lore(final List<String> lore){
-		final ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = this.item.getItemMeta();
 		meta.setLore(lore);
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 	
 	public ItemBuilder coloredLore(final List<String> lore){
-		final ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = this.item.getItemMeta();
 		meta.setLore(Colors.parseColors(lore));
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 	
 	public ItemBuilder skullOwner(final OfflinePlayer player){
-		final SkullMeta meta = (SkullMeta) item.getItemMeta();
+		final SkullMeta meta = (SkullMeta) this.item.getItemMeta();
 		meta.setOwningPlayer(player);
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 
 	public ItemBuilder leatherArmorColor(final Color color){
-		final LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
+		final LeatherArmorMeta meta = (LeatherArmorMeta) this.item.getItemMeta();
 		meta.setColor(color);
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 	
 	public ItemBuilder enchant(final Enchantment type, final int level){
-		item.addEnchantment(type, level);
+		this.item.addEnchantment(type, level);
 		return this;
 	}
 	
 	public ItemBuilder unsafeEnchant(final Enchantment type, final int level){
-		final ItemMeta meta = item.getItemMeta();
-		meta.addEnchant(type, level, true);
+		this.item.addUnsafeEnchantment(type, level);
 		return this;
 	}
 	
 	public ItemBuilder material(final Material material){
-		item.setType(material);
+		this.item.setType(material);
 		return this;
 	}
 	
 	public ItemBuilder type(final Material type){
-		item.setType(type);
+		this.item.setType(type);
 		return this;
 	}
 	
 	public ItemBuilder unbreakable() {
-		final ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = this.item.getItemMeta();
 		meta.setUnbreakable(true);
-		item.setItemMeta(meta);
+		this.item.setItemMeta(meta);
 		return this;
 	}
 	
 	public ItemBuilder canDestroy(final String... minecraftItemNames) {
-		item = ReflectionUtil.addCanDestroy(item, minecraftItemNames);
+		this.item = ReflectionUtil.addCanDestroy(this.item, minecraftItemNames);
 		return this;
 	}
 	
 	public ItemBuilder canPlaceOn(final String... minecraftItemNames) {
-		item = ReflectionUtil.addCanPlaceOn(item, minecraftItemNames);
+		this.item = ReflectionUtil.addCanPlaceOn(this.item, minecraftItemNames);
 		return this;
 	}
 	
 	public ItemBuilder damage(final int damage) {
-		final Damageable damageable = (Damageable) item.getItemMeta();
+		final Damageable damageable = (Damageable) this.item.getItemMeta();
 		damageable.setDamage(damage);
-		item.setItemMeta((ItemMeta) damageable);
+		this.item.setItemMeta((ItemMeta) damageable);
 		return this;
 	}
 	
+	public ItemBuilder hideFlags(final int hideFlags) {
+		final NBTItem nbt = new NBTItem(this.item);
+		nbt.setInteger("HideFlags", hideFlags);
+		this.item = nbt.getItem();
+		return this;
+	}
+	
+	public ItemBuilder namePlaceholder(final String key, final String value) {
+		return this.name(this.item.getItemMeta().getDisplayName().replace(key, value));
+	}
+
+	public ItemBuilder namePlaceholders(final Map<String, String> placeholders) {
+		placeholders.forEach(this::namePlaceholder);
+		return this;
+	}
+	
+	public ItemBuilder namePlaceholderOptional(final String key, final Supplier<String> value) {
+		final String oldName = this.item.getItemMeta().getDisplayName();
+		if (oldName.contains(key))
+			return this.name(oldName.replace(key, value.get()));
+		else
+			return this;
+	}
+	
+	public ItemBuilder namePlaceholdersOptional(final Map<String, Supplier<String>> placeholders) {
+		placeholders.forEach(this::namePlaceholderOptional);
+		return this;
+	}
+	
+	public ItemBuilder lorePlaceholder(final String key, final String value) {
+		return this.lore(this.item.getItemMeta().getLore().stream().map((s) -> s.replace(key, value)).collect(Collectors.toList()));
+	}
+	
+	public ItemBuilder lorePlaceholders(final Map<String, String> placeholders) {
+		placeholders.forEach(this::lorePlaceholder);
+		return this;
+	}
+	
+	public ItemBuilder lorePlaceholderOptional(final String key, final Supplier<String> value) {
+		return this.lore(this.item.getItemMeta().getLore().stream().map((s) -> {
+			if (s.contains(key))
+				return s.replace(key, value.get());
+			else
+				return s;
+		}).collect(Collectors.toList()));
+	}
+	
+	public ItemBuilder lorePlaceholdersOptional(final Map<String, Supplier<String>> placeholders) {
+		placeholders.forEach(this::lorePlaceholderOptional);
+		return this;
+	}
+	
+	public ItemBuilder placeholder(final String key, final String value) {
+		return this.namePlaceholder(key, value).lorePlaceholder(key, value);
+	}
+	
+	public ItemBuilder placeholders(final Map<String, String> placeholders) {
+		return this.namePlaceholders(placeholders).lorePlaceholders(placeholders);
+	}
+	
+	public ItemBuilder placeholderOptional(final String key, final Supplier<String> value) {
+		return this.namePlaceholderOptional(key, value).lorePlaceholderOptional(key, value);
+	}
+	
+	public ItemBuilder placeholdersOptional(final Map<String, Supplier<String>> placeholders) {
+		return this.namePlaceholdersOptional(placeholders).lorePlaceholdersOptional(placeholders);
+	}
+	
+	public ItemBuilder lorePapi(final Player player) {
+		return this.lore(this.item.getItemMeta().getLore().stream().map((s) -> PlaceholderUtil.parsePapiPlaceholders(player, s)).collect(Collectors.toList()));
+	}
+	
+	public ItemBuilder namePapi(final Player player) {
+		return this.name(PlaceholderUtil.parsePapiPlaceholders(player, this.item.getItemMeta().getDisplayName()));
+	}
+	
+	public ItemBuilder papi(final Player player) {
+		this.namePapi(player);
+		return this.lorePapi(player);
+	}
+	
 	public ItemStack create(){
-		return item;
+		return this.item;
 	}
 
 }
