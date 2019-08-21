8301a0b78831925f0fb175055fcf15f6c8478c55
Robin <robinslot@hotmail.nl>
2019-08-21 02:43:16 +0200

    Fix NPE

--- src/xyz/derkades/derkutils/bukkit/IllegalItems.java
+++ src/xyz/derkades/derkutils/bukkit/IllegalItems.java
@@ -14,33 +14,33 @@ import org.bukkit.plugin.Plugin;
 import de.tr7zw.nbtapi.NBTItem;
 
 public class IllegalItems implements Listener {
-	
+
 	private static final String NBT_KEY = "DerkutilsIllegalItem";
-	
+
 	private final Plugin plugin;
 	private final boolean debug;
-	
+
 	public IllegalItems(final Plugin plugin) {
 		this(plugin, false);
 	}
-	
+
 	public IllegalItems(final Plugin plugin, final boolean debug) {
 		this.plugin = plugin;
 		this.debug = debug;
-		
+
 		Bukkit.getPluginManager().registerEvents(this, plugin);
-		
+
 		if (debug) {
 			plugin.getLogger().info("Illegal items registered");
 		}
 	}
-	
+
 	public ItemStack setIllegal(final ItemStack item, final boolean illegal) {
 		final NBTItem nbt = new NBTItem(item);
 		nbt.setBoolean(NBT_KEY, illegal);
 		return nbt.getItem();
 	}
-	
+
 	public boolean isIllegal(final ItemStack item) {
 		final NBTItem nbt = new NBTItem(item);
 		if (!nbt.hasNBTData())
@@ -50,13 +50,17 @@ public class IllegalItems implements Listener {
 		else
 			return nbt.getBoolean(NBT_KEY);
 	}
-	
+
 	public void unregister() {
 		HandlerList.unregisterAll(this);
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onInventoryClick(final InventoryClickEvent event) {
+		if (event.getClickedInventory() == null) { // when clicking outside of the inventory while it is open
+			return;
+		}
+
 		if (event.getClickedInventory().getType() == InventoryType.PLAYER) {
 			final ItemStack item = event.getCurrentItem();
 			if (this.isIllegal(item)) {
