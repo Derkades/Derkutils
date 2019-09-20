04d78ba88b1baa4de1c8f86e14ffa1af5af91254
Robin <robinslot@hotmail.nl>
2019-09-20 14:33:03 +0200

    Fixed phrasing

--- src/xyz/derkades/derkutils/NoParameter.java
+++ src/xyz/derkades/derkutils/NoParameter.java
@@ -2,7 +2,7 @@ package xyz.derkades.derkutils;
 
 /**
  * Class to be used as a generic type when you don't want to return anything. Can be used in for example {@link xyz.derkades.derkutils.bukkit.BukkitFuture}.
- * <br><br>If an instance of this object is returned, assume it is null.
+ * <br><br>Assume any variable with this type is null.
  */
 public class NoParameter {
 
