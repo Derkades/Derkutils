eeec7b7144a31f4f5e9224f49b1bbf006b2fb950
Robin <robinslot@hotmail.nl>
2021-02-17 16:13:53 +0100

    Add validation to database handler

--- src/xyz/derkades/derkutils/DatabaseHandler.java
+++ src/xyz/derkades/derkutils/DatabaseHandler.java
@@ -8,12 +8,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import org.apache.commons.lang.Validate;
+
 public class DatabaseHandler {
 
 	private final boolean debug;
 	private final Connection databaseConnection;
 
 	public DatabaseHandler(final String host, final int port, final String database, final String user, final String password, final boolean debug) throws SQLException {
+		Validate.notNull(host, "Host is null");
+		Validate.isTrue(port > 0 && port < 65535, "Port out of range");
+		Validate.notNull(database, "Database name is null");
+		Validate.notNull(user, "Username is null");
+		Validate.notNull(password, "Password is null");
+		
 		this.debug = debug;
 
 		final Properties properties = new Properties();
@@ -30,6 +38,9 @@ public class DatabaseHandler {
 	}
 
 	public PreparedStatement prepareStatement(final String sql, final Object... parameters) throws SQLException {
+		Validate.notNull(sql, "Query is null");
+		Validate.notNull(parameters, "Parameters varargs is null");
+		
 		if (this.debug) {
 			System.out.println("Making query: " + sql);
 			for (final Object parameter : parameters) {
@@ -58,6 +69,9 @@ public class DatabaseHandler {
 	}
 	
 	public void createTableIfNonexistent(final String table, final String sql) throws SQLException {
+		Validate.notNull(table, "Table is null");
+		Validate.notNull(sql, "Query is null");
+		
 		final DatabaseMetaData meta = this.getConnection().getMetaData();
 		final ResultSet result = meta.getTables(null, null, table, null);
 
