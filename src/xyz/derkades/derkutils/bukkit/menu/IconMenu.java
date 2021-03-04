5096bcbdd3b4699f1b0667ee5f83ab8aaf01bb22
Robin <robinslot@hotmail.nl>
2021-03-04 21:00:51 +0100

    use lang3

--- src/xyz/derkades/derkutils/bukkit/menu/IconMenu.java
+++ src/xyz/derkades/derkutils/bukkit/menu/IconMenu.java
@@ -2,7 +2,7 @@ package xyz.derkades.derkutils.bukkit.menu;
 
 import java.util.function.Consumer;
 
-import org.apache.commons.lang.Validate;
+import org.apache.commons.lang3.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
