103df2375f9d306a7cef8ec3fc7bfaf372171f66
Robin <robinslot@hotmail.nl>
2021-03-21 20:55:49 +0100

    add cornermin/cornermax methods

--- src/xyz/derkades/derkutils/bukkit/LocationUtils.java
+++ src/xyz/derkades/derkutils/bukkit/LocationUtils.java
@@ -1,5 +1,6 @@
 package xyz.derkades.derkutils.bukkit;
 
+import org.apache.commons.lang3.Validate;
 import org.bukkit.Location;
 
 public class LocationUtils {
@@ -20,17 +21,27 @@ public class LocationUtils {
 		return x >= minX && x <= maxX &&
 				z >= minZ && z <= maxZ;
 	}
-	
+
 	public static boolean isIn3dBounds(final Location location, final Location cornerOne, final Location cornerTwo) {
 		if (!isIn2dBounds(location, cornerOne, cornerTwo)) {
 			return false;
 		}
-		
+
 		final double maxY = Math.max(cornerOne.getY(), cornerTwo.getY());
 		final double minY = Math.min(cornerOne.getY(), cornerTwo.getY());
 		final double y = location.getY();
-		
+
 		return y >= minY && y <= maxY;
+	}
+
+	public static Location maxCorner(final Location a, final Location b) {
+		Validate.isTrue(a.getWorld().equals(b.getWorld()), "Locations must be in the same world");
+		return new Location(a.getWorld(), Math.max(a.getX(), b.getX()), Math.max(a.getY(), b.getY()), Math.max(a.getZ(), b.getZ()));
+	}
+
+	public static Location minCorner(final Location a, final Location b) {
+		Validate.isTrue(a.getWorld().equals(b.getWorld()), "Locations must be in the same world");
+		return new Location(a.getWorld(), Math.max(a.getX(), b.getX()), Math.max(a.getY(), b.getY()), Math.max(a.getZ(), b.getZ()));
 	}
 
 }
